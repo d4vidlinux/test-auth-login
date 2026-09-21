ad99d5994c31d859717e2446cf3b6e527328ba73
David <davidlucaspereirapinto06@gmail.com>
2026-09-20 21:11:47 -0300

    login_selenium

--- login_selenium.py
+++ login_selenium.py
@@ -33,5 +33,40 @@ password.send_keys("secret_sauce")
 # click
 btn_login.click()
 
+wait.until(
+    EC.presence_of_element_located((By.XPATH, "//div[@class='inventory_item_description']"))
+)
+
+browser.find_element(By.XPATH, "//div[text()='Sauce Labs Backpack']").click()
+
+wait.until(
+    EC.element_to_be_clickable((By.ID, "add-to-cart"))
+).click()
+
+wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//a[@class='shopping_cart_link']"))
+).click()
+
+wait.until(
+    EC.element_to_be_clickable((By.ID, "checkout"))
+).click()
+
+first_name = wait.until(
+    EC.presence_of_element_located((By.ID, "first-name"))
+)
+
+last_name = browser.find_element(By.ID, "last-name")
+postal_code = browser.find_element(By.ID, "postal-code")
+
+first_name.send_keys("d4vid")
+last_name.send_keys("linux")
+postal_code.send_keys("0000")
+
+browser.find_element(By.ID, "continue").click()
+
+finish = wait.until(
+    EC.element_to_be_clickable((By.ID, "finish"))
+).click()
+
 input("Press Enter to exit...")
 browser.quit()
